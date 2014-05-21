83008eb686fcedb13a68ade6f8a15aff434ae5e3
Rachael Serur <rachaelse@gmail.com>
2014-05-21 10:20:39 -0400

    Added unique position names to index page.

--- server.rb
+++ server.rb
@@ -21,16 +21,20 @@ get '/' do
   team_array = get_rosters
 
   team_names = []
+  pos_names = []
 
   team_array.each do |row|
 
     team_names.push(row[:team])
+    pos_names.push(row[:position])
 
   end
 
   team_names.uniq!
+  pos_names.uniq!
 
   @teams = team_names
+  @positions = pos_names
   erb :index
 
 end
