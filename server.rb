8618d672f586dbe0ea1ac79aabf68ac68462702e
Rachael Serur <rachaelse@gmail.com>
2014-05-21 00:30:45 -0400

    Tidying up code w/CSV method, added last names.

--- server.rb
+++ server.rb
@@ -4,30 +4,7 @@ require 'sinatra'
 require 'csv'
 require 'uri'
 
-get '/' do
-
-  team_array = []
-
-  CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-    team_array << row.to_hash
-  end
-
-
-  team_names = []
-
-  team_array.each do |row|
-
-    team_names.push(row["team"])
-
-  end
-
-  team_names.uniq!
-
-  @teams = team_names
-  erb :index
-end
-
-get '/:team' do
+def get_names
 
   team_array = []
 
@@ -35,16 +12,37 @@ get '/:team' do
     team_array << row.to_hash
   end
 
-  @teams = team_array
+  team_array
+
+end
+
+get '/' do
+
+  team_array = get_names
+
+  team_names = []
+
+  team_array.each do |row|
+
+    team_names.push(row[:team])
+
+  end
+
+  team_names.uniq!
+
+  @teams = team_names
+  erb :index
+
+end
+
+get '/:team' do
+
+  @teams = get_names
 
   @teams = @teams.find_all do |t|
     t[:team] == params[:team]
   end
 
   erb :show
-end
 
-# These lines can be removed since they are using the default values. They've
-# been included to explicitly show the configuration options.
-set :views, File.dirname(__FILE__) + '/views'
-set :public_folder, File.dirname(__FILE__) + '/public'
+end
